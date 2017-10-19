9b901990e37dc9d9ef3ebd45ac6b1aeca153a35e
leah duvic <leaheduvic@gmail.com>
2017-10-19 14:38:23 -0500

    messed with css and created new js files

---
+++ blogData.js
@@ -0,0 +1,38 @@
+const blogData = [
+    {
+        "Btitle": "Week 1 has come and gone.",
+        "date": "10/06/2017",
+        "number": "1",
+        "post": "",
+    },
+    {
+        "Btitle": "Week 2",
+        "date": "10/13/2017",
+        "number": "2",
+        "post": "This week went really well. We started our first experiences with group projects. So far that is going well, haven't run into any major issues. We learned the whole terminal and github routine, which i am slowly catching on to.",
+    },
+    {
+        "Btitle": "",
+        "date": "10/20/2017",
+        "number": "3",
+        "post": "",
+    },
+    {
+        "Btitle": "",
+        "date": "",
+        "number": "4",
+        "post": ""
+    },
+    {
+        "Btitle": "",
+        "date": "",
+        "number": "5",
+        "post": ""
+    },
+    {
+        "Btitle": "",
+        "date": "",
+        "number": "6",
+        "post": ""
+    }
+]
